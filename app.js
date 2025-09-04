fa6c6fa3457a30bb647f191147d5811616045dc7
UserLab-Teia <Git.Hub@teia.mx>
2025-09-04 17:12:03 +0000

    fix(validation): Add new validations

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ window.addEventListener('DOMContentLoaded', function () {
             edad: parseInt(document.getElementById('edad').value, 10),
             genero: document.getElementById('genero').value
         };
-        if (!usuario.nombre || !usuario.apellidoPaterno || isNaN(usuario.edad) || !usuario.genero) {
-            resultado.textContent = 'Por favor, complete todos los campos correctamente.';
-            resultado.style.color = 'red';
-            return;
-        }
+            var nombreValido = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/.test(usuario.nombre);
+            var apellidoValido = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/.test(usuario.apellidoPaterno);
+            if (!usuario.nombre || !usuario.apellidoPaterno || isNaN(usuario.edad) || !usuario.genero || !nombreValido || !apellidoValido) {
+                resultado.textContent = 'Por favor, complete todos los campos correctamente. Nombre y Apellido no deben contener números.';
+                resultado.style.color = 'red';
+                return;
+            }
         resultado.textContent = "Registro exitoso: ".concat(usuario.nombre, " ").concat(usuario.apellidoPaterno, ", Edad: ").concat(usuario.edad, ", Género: ").concat(usuario.genero);
         resultado.style.color = '#0078d4';
     });
